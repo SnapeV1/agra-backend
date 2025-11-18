bd789c845295c336e3c6c7789447d2ba3f674821
snapev1 <hamada_amine@yahoo.com>
2025-11-18 23:43:53 +0100

    fixing test env error

--- src/test/java/org/agra/agra_backend/AgraBackendApplicationTests.java
+++ src/test/java/org/agra/agra_backend/AgraBackendApplicationTests.java
@@ -2,8 +2,7 @@ package org.agra.agra_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class AgraBackendApplicationTests {
 
 	@Test
