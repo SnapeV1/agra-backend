8003bbc3cf59009b06bd1b46ba91631d112b39ac
snapev1 <hamada_amine@yahoo.com>
2025-12-20 15:06:30 +0100

    tests

--- agra-backend/src/test/java/org/agra/agra_backend/AgraBackendApplicationTests.java
+++ agra-backend/src/test/java/org/agra/agra_backend/AgraBackendApplicationTests.java
@@ -1,8 +1,10 @@
 package org.agra.agra_backend;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Tag("integration")
 class AgraBackendApplicationTests {
 
 	@Test
