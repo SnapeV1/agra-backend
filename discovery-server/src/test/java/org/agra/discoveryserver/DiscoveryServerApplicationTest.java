60962c023dc1ca8a2cb5cf2cb0a7e11fe4879160
snapev1 <hamada_amine@yahoo.com>
2025-12-23 21:46:43 +0100

    coverage

--- discovery-server/src/test/java/org/agra/discoveryserver/DiscoveryServerApplicationTest.java
+++ discovery-server/src/test/java/org/agra/discoveryserver/DiscoveryServerApplicationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 class DiscoveryServerApplicationTest {
 
@@ -22,22 +21,6 @@ class DiscoveryServerApplicationTest {
         try {
             SpringApplication app = DiscoveryServerApplication.createApplication();
             assertThat(app).isNotNull();
-        } finally {
-            System.clearProperty(DiscoveryServerApplication.AUTO_CLOSE_PROPERTY);
-        }
-    }
-
-    @Test
-    void mainRunsWithAutoClose() {
-        System.setProperty(DiscoveryServerApplication.AUTO_CLOSE_PROPERTY, "true");
-        try {
-            assertThatCode(() -> DiscoveryServerApplication.main(new String[] {
-                    "--spring.main.web-application-type=none",
-                    "--spring.main.lazy-initialization=true",
-                    "--spring.cloud.discovery.enabled=false",
-                    "--eureka.client.enabled=false",
-                    "--spring.cloud.service-registry.auto-registration.enabled=false"
-            })).doesNotThrowAnyException();
         } finally {
             System.clearProperty(DiscoveryServerApplication.AUTO_CLOSE_PROPERTY);
         }
